777223bbfe1942374f484bc75517f1060bedbd1a
森田陸斗 <mrmrt1121@gmail.com>
2022-03-23 20:57:12 +0900

    Rowを追加

--- lib/layout_practice.dart
+++ lib/layout_practice.dart
@@ -26,3 +26,46 @@ class ColumnPractice extends StatelessWidget {
     );
   }
 }
+
+/// Row Widget
+class RowPractice extends StatelessWidget {
+  const RowPractice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Container(
+          width: 100,
+          // height: 100, CrossAxisAlignment.stretchのため設定なし
+          child: const Text('child1'),
+          color: Colors.red,
+        ),
+        Container(
+          width: 50,
+          // height: 50,　CrossAxisAlignment.stretchのため設定なし
+          child: const Text('child2'),
+          color: Colors.blue,
+        ),
+      ],
+    );
+  }
+}
+
+
+/// Center Widget
+class CenterPractice extends StatelessWidget {
+  const CenterPractice({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  Center(
+      child: Container (
+          color: Colors.deepOrangeAccent,
+          child: const Text('Center Widgetでchildを真ん中に')
+      ),
+    );
+  }
+}
